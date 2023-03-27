373fa3be592d9e5078eae7ea307bfbaa3f7e608f
Samet Erkan Boz <71569375+sameterkanboz@users.noreply.github.com>
2023-03-27 11:43:15 +0300

    push

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	// "github.com/sameterkanboz/hotelManagement/database"
+	"github.com/sameterkanboz/hotelManagement/database"
 )
 
 func main() {
-	// database.ConnectDb()
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
